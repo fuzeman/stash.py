7c974ec334f2fb28bcce404ef2b1af94c57d01f7
Dean Gardiner <gardiner91@gmail.com>
2015-03-18 11:33:55 +1300

    Implemented `ApswArchive`

---
+++ examples/apsw_archive.py
@@ -0,0 +1,31 @@
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+from stash import Stash
+
+
+if __name__ == '__main__':
+    s = Stash('apsw:///apsw_archive.db?table=stash', 'lru:///?capacity=5')
+
+    for x in xrange(5):
+        s[str(x)] = x
+
+    for x in xrange(2):
+        s[str(x)] = x
+
+    for x in xrange(10, 13):
+        s[str(x)] = x
+
+    s.flush()
+
+    print 'len(s.cache): %r' % len(s.cache)
+    print 'len(s.archive): %r' % len(s.archive)
+
+    for x in xrange(5):
+        assert s[str(x)] == x
+
+    print "del s['1']"
+    del s['1']
+
+    print 'len(s.cache): %r' % len(s.cache)
+    print 'len(s.archive): %r' % len(s.archive)
--- stash/archives/a_apsw.py
+++ stash/archives/a_apsw.py
@@ -1,5 +1,69 @@
 from stash.archives.core.base import Archive
 
+from contextlib import closing
+import apsw
+
 
 class ApswArchive(Archive):
     __key__ = 'apsw'
+
+    def __init__(self, db, table):
+        super(ApswArchive, self).__init__()
+
+        self.db = apsw.Connection(db) if type(db) is str else db
+        self.table = table
+
+        # Ensure table exists
+        with closing(self.db.cursor()) as c:
+            c.execute('create table if not exists "%s" (key PRIMARY KEY, value)' % self.table)
+
+    def select(self, sql, parameters=None):
+        if parameters is None:
+            parameters = ()
+
+        with closing(self.db.cursor()) as c:
+            return list(c.execute(sql, parameters))
+
+    def select_one(self, sql, parameters=None):
+        rows = self.select(sql, parameters)
+
+        if not rows:
+            return None
+
+        return rows[0]
+
+    def __delitem__(self, key):
+        with closing(self.db.cursor()) as c:
+            result = c.execute('delete from "%s" where key=?' % self.table, key)
+            rows = list(result)
+
+            success = len(rows) > 0
+
+        if not success:
+            raise KeyError(key)
+
+    def __getitem__(self, key):
+        row = self.select_one('select value from "%s" where key=?' % self.table, (key, ))
+
+        if not row:
+            raise KeyError(key)
+
+        return self.loads(row[0])
+
+    def __iter__(self):
+        raise NotImplementedError
+
+    def __len__(self):
+        row = self.select_one('select count(*) from "%s"' % self.table)
+
+        if not row:
+            return None
+
+        return row[0]
+
+    def __setitem__(self, key, value):
+        value = self.dumps(value)
+
+        with closing(self.db.cursor()) as c:
+            c.execute('update "%s" set value=? WHERE key=?' % self.table, (value, key))
+            c.execute('insert or ignore into "%s" values(?,?)' % self.table, (key, value))
